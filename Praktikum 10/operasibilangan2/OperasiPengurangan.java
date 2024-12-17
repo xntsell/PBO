c1c36b105122b32f5b97e208b42a2e379f320a9c
Chrisella <141915616+xntsell@users.noreply.github.com>
2024-12-17 09:57:05 +0700

    Delete Praktikum 10/operasibilangan2 directory

--- Praktikum 10/operasibilangan2/OperasiPengurangan.java
+++
@@ -1,9 +0,0 @@
-package operasibilangan2;
-
-// Class OperasiPengurangan
-class OperasiPengurangan extends OperasiBilangan {
-  @Override
-  protected void set_C() {
-    c = a - b;
-  }
-}
--- Praktikum 10/operasibilangan2/OperasiPerkalian.java
+++
@@ -1,9 +0,0 @@
-package operasibilangan2;
-
-// Class OperasiPerkalian
-class OperasiPerkalian extends OperasiBilangan {
-  @Override
-  protected void set_C() {
-    c = a * b;
-  }
-}
